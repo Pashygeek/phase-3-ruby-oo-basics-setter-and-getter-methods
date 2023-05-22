9e62f5c6147b4b5da2791b0743f93216415be07a
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-22 13:13:03 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
-
+# setter method
+def name=(dog_name)
+    @name = dog_name
 end
+
+# getter method
+def name
+    @name
+end
+
+def breed=(dog_breed)
+    @breed = dog_breed
+end
+
+def breed
+    @breed
+end
+end
+
+# Example of usage and output
+pitbull = Dog.new
+pitbull.name = "Pitbull"
+pitbull.breed = "Labrador"
+
+puts pitbull.name
+puts pitbull.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
-
+    def name=(person_name)
+        @name = person_name
 end
+
+def name
+    @name
+end
+
+def job=(person_job)
+    @job = person_job
+end
+
+def job
+    @job
+end
+end
+
+# Example usage and output
+pashy = Person.new
+pashy.name = "Pashy"
+pashy.job = "Software engineer"
+
+puts pashy.name
+puts pashy.job
